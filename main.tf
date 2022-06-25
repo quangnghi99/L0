25ed5a2f25ad96551929c1331fc2b0e494b6e7c2
quangnghi99 <84608260+quangnghi99@users.noreply.github.com>
2022-06-25 17:07:14 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "ec2-instance" {
     source = "./ec2-instance"
     AWS_REGION = "eu-central-1"
-    ami_id = "ami-01d9d7f15bbea00b7"
+    ami_id = "ami-019afd3459f39e677"
     instance_type = "t2.micro"
     cidr_blocks = ["0.0.0.0/0"]
 }
