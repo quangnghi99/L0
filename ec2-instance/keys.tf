7fa0f9d34d7f8d4ee4617f16b0002bb1d569b0eb
quangnghi99 <84608260+quangnghi99@users.noreply.github.com>
2022-06-25 17:05:16 +0700

    Update keys.tf

--- ec2-instance/keys.tf
+++ ec2-instance/keys.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "nghi_key" {
     key_name = "nghi_tf_key"
-    public_key = file("/home/anhdo/TH_terraform/dev-vpc_test/ssh-keys/anhdo-key.pub")
+    public_key = file("/home/ec2-user/nghi_tf_key.pub")
 }
